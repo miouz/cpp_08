ffb778efa69c019b67119b4c4edc727b146167ce
Zhou Mi <mizhouis@gmail.com>
2026-01-19 13:55:50 +0100

    add(ex02): add tests for const iterator

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -53,5 +53,33 @@ int main()
 	}
 	std::stack<int, std::list<int> > s(mstack);
 	}
+
+	{
+	std::cout << "test for const_iterator";
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	MutantStack<int>::const_iterator it = mstack.cbegin();
+	MutantStack<int>::const_iterator ite = mstack.cend();
+	++it;
+	--it;
+	// *it = 9;
+	//*ite = 10;
+	while (it != ite)
+	{
+	std::cout << *it << std::endl;
+	++it;
+	}
+	std::stack<int> s(mstack);
+	}
+
 	return 0;
 }
