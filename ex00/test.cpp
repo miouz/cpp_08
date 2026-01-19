335444810bfcc92ccd22ee2c4e0fb29fe0f341c7
Mi Zhou <mzhou@student.42.fr>
2026-01-19 14:35:34 +0100

    add(ex00): add test files

--- ex00/test.cpp
+++ ex00/test.cpp
@@ -1,6 +1,7 @@
 #include "test.hpp"
 #include <list>
-#include <forward_list>
+#include <vector>
+#include <deque>
 
 template<typename T>
 void printContainer(T& container)
@@ -29,19 +30,9 @@ void testEx00()
 	std::cout << "after founded " << *found1 <<" is " << *(++found1) << std::endl;
 
 	PRINT_SUBHEADER("occurence presents 0 time");
-	std::vector<int>::iterator found2 = easyFind(intVec, 100);
+	std::vector<int>::iterator found2 = easyFind(intVec, -4);
 	if (found2 == intVec.end())
 		std::cout << "didn't find 100 because found2's value is vector's end: "<< *found2 << std::endl;
-	}
-
-	{
-	PRINT_HEADER(" ARRAY CONTAINER OF INT");
-	std::array<int,5>intArr = {1, 908, -43, 56, -43};
-	PRINT_SUBHEADER("array container contains: ");
-	printContainer(intArr);
-	PRINT_SUBHEADER("occurence presents 2 times");
-	std::array<int, 5>::iterator found = easyFind(intArr, -43);
-	std::cout << "after founded " << *found <<" is " << *(found + 1)<< std::endl;
 	}
 
 	{
@@ -54,12 +45,20 @@ void testEx00()
 	intDeq.insert(intDeq.begin() + 2, -43);
 	PRINT_SUBHEADER("deque container contains: ");
 	printContainer(intDeq);
+	
+	PRINT_SUBHEADER("occurence presents 1 time");
+	std::deque<int>::iterator found1 = easyFind(intDeq, 908);
+	std::cout << *found1 << std::endl;
+
+	PRINT_SUBHEADER("occurence presents 2 times");
+	std::deque<int>::iterator found2 = easyFind(intDeq, -43);
+	std::cout << "after founded " << *found2 <<" is " << *(++found2)<< std::endl;
+
 	PRINT_SUBHEADER("occurence presents 0 times");
 	std::deque<int>::iterator found = easyFind(intDeq, -4);
 	if (found == intDeq.end())
-		std::cout << "didn't find 100 because found's value is vector's end: "<< *found << std::endl;
+		std::cout << "didn't find -4 because found's value is vector's end: "<< *found << std::endl;
 	}
-
 
 	{
 	PRINT_HEADER("LIST CONTAINER OF INT");
@@ -74,25 +73,18 @@ void testEx00()
 	intList.insert(it, -43);
 	PRINT_SUBHEADER("list container contains: ");
 	printContainer(intList);
-	PRINT_SUBHEADER("occurence presents 2 times");
-	std::list<int>::iterator found = easyFind(intList, -43);
-	std::cout << "after founded " << *found <<" is " << *(++found)<< std::endl;
-	}
 
+	PRINT_SUBHEADER("occurence presents 1 time");
+	std::list<int>::iterator found1 = easyFind(intList, 908);
+	std::cout << *found1 << std::endl;
 
-	{
-	PRINT_HEADER("FORWARDLIST CONTAINER OF INT");
-	std::forward_list<int> intList;
-	intList.push_front(1);
-	intList.insert_after(intList.begin(), 908);
-	std::forward_list<int>::iterator it = intList.begin();
-	intList.insert_after(++it, -43);
-	intList.insert_after(++it, 56);
-	intList.insert_after(++it, -43);
-	PRINT_SUBHEADER("forwad_list container contains: ");
-	printContainer(intList);
 	PRINT_SUBHEADER("occurence presents 2 times");
-	std::forward_list<int>::iterator found = easyFind(intList, -43);
-	std::cout << "after founded " << *found <<" is " << *(++found)<< std::endl;
+	std::list<int>::iterator found2 = easyFind(intList, -43);
+	std::cout << "after found " << *found2 <<" is " << *(++found2)<< std::endl;
+
+	PRINT_SUBHEADER("occurence presents 0 times");
+	std::list<int>::iterator found = easyFind(intList, -4);
+	if (found == intList.end())
+		std::cout << "didn't find -4 because found's value is vector's end: "<< *found << std::endl;
 	}
 }
